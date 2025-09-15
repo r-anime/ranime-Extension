c5167633a32b7e17135ffc42b600ad1995a9428a
Philip Ross <dev@baseballlover723.com>
2025-09-14 18:38:55 -0700

    Merge extensions together and directly parse css (#2)
    * created basic merged extension
    
    * remove old spoiler support
    
    * update readme
    
    * lint
    
    * parse css directly

---
+++ Merged/options.js
@@ -0,0 +1,52 @@
+// Use browser.* if available, otherwise fall back to chrome.*
+if (typeof browser === 'undefined') {
+  var browser = chrome;
+}
+
+function saveOptions(e) {
+  e.preventDefault();
+  browser.storage.local.set({
+    opcommentfaces: document.querySelector('#commentfaces').checked,
+    opanisearch: document.querySelector('#anisearch').checked,
+    opspoiler: document.querySelector('#spoiler').checked,
+    nuspoiler: document.querySelector('#nuspoiler').checked,
+  });
+}
+
+function restoreOptions() {
+  function setCurrentChoice(result) {
+    if (result.opcommentfaces !== undefined) {
+      document.querySelector('#commentfaces').checked = result.opcommentfaces;
+    } else {
+      document.querySelector('#anisearch').checked = true;
+    }
+
+    if (result.opanisearch !== undefined) {
+      document.querySelector('#anisearch').checked = result.opanisearch;
+    } else {
+      document.querySelector('#anisearch').checked = true;
+    }
+
+    if (result.opspoiler !== undefined) {
+      document.querySelector('#spoiler').checked = result.opspoiler;
+    } else {
+      document.querySelector('#spoiler').checked = false;
+    }
+
+    if (result.nuspoiler !== undefined) {
+      document.querySelector('#nuspoiler').checked = result.nuspoiler;
+    } else {
+      document.querySelector('#nuspoiler').checked = false;
+    }
+  }
+
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+
+  const getting = browser.storage.local.get(['opcommentfaces', 'opanisearch', 'opspoiler', 'nuspoiler']);
+  getting.then(setCurrentChoice, onError);
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector('form').addEventListener('submit', saveOptions);
--- README.md
+++ README.md
@@ -4,8 +4,10 @@
 
 This Mozilla Firefox and Google Chrome browser Extension for the subreddits [r/anime](https://www.reddit.com/r/anime) and [r/manga](https://www.reddit.com/r/manga) enhances the experience of the subreddit. It enables easy search and use for commentfaces, easy search and insertion of library entries via AniList (anime, manga, staff members, characters, studios) and a comfortable Spoiler-Tag-button.
 
+Newly taken over by the r/anime mod team and u/baseballlover723 specifically.
+
 ### Get the Extension
 
-[Firefox](https://addons.mozilla.org/en-US/firefox/addon/r-anime-enhanced/)
+[Firefox](https://addons.mozilla.org/en-US/firefox/addon/r-anime-enhanced-2/)
 
-[Chrome](https://chrome.google.com/webstore/detail/ranime-enhanced/fpgkklfainkaakkgihcfdgeglleogkhc)
+[Chrome](https://chromewebstore.google.com/detail/ranime-enhanced/kamojikipljapehmpckboobelllakilh)
